1341d42ad5560837b3b3659a8c04e008012c9028
Wonsuk oh <wsoh51@gmail.com>
2023-06-10 11:57:10 +0900

    lab10 index.js에서 리듀서 등록

--- lab10/src/index.js
+++ lab10/src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
+import postListReduer from './slices/PostListSlice';
 
 // RTK(Redux Toolkit)리덕스 스토어 객체 생성
 const store = configureStore({
-  reducer: {}, // TODO 필요한 리듀서 객체를 추가함
+  reducer: {
+    postList: postListReduer,
+  }, // TODO 필요한 리듀서 객체를 추가함
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
